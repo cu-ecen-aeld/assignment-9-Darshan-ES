d0b4baaa16658accad690f8798cd6d03fee966da
darshanES <darshanes@Ubuntu2.myguest.virtualbox.org>
2025-02-09 09:53:16 -0700

    clean.sh changed

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "distclean Buildroot Directory"
+echo "distclean Buildroot"
 cd buildroot || { echo "Not Found BuildRoot Directory"; exit 1; }
 make distclean
 echo "Buildroot cleaned."
